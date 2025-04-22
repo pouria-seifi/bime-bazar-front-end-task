856ee9136c0cd0f2087a7db358d163cce28ef6f3
Pouria <82539162+Pouria1378@users.noreply.github.com>
2025-04-23 00:10:14 +0330

    add insurance policy details in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,58 @@
 import Image from "next/image";
 
+import Logo from "@/public/images/logo.svg";
+import CarPlate from "@/public/images/carPlate.svg";
+
+import { Typography } from "@/src/components/typography";
+import { COLORS, FONT_SIZE, FONT_WEIGHT } from "@/src/enums/tailwind.enum";
+import Header from "@/src/components/header";
+import { LabelValuePairs } from "@/src/types";
+
+const insurancePolicyDetails: LabelValuePairs = [
+  { label: "شرکت بیمه گر", value: "پارسیان" },
+  { label: "برند خودرو", value: "پژو" },
+  { label: "مدل خودرو", value: "206 تیپ 6" },
+];
+
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <div className="min-h-screen">
+      <main>
+        <section>
+          <Header title="مشخصات بیمه نامه" src={Logo} />
+          <div className="flex flex-col justify-center px-10 pt-6">
+            <div className="relative h-[50px]">
+              <Image className="" src={CarPlate} alt="car plate image" fill />
+            </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+            <div className="flex flex-col mt-6 gap-2">
+              {insurancePolicyDetails.map(({ label, value }, index) => (
+                <div
+                  key={index}
+                  className="flex flex-row justify-between items-center"
+                >
+                  <Typography.Text
+                    weight={FONT_WEIGHT.regular}
+                    size={FONT_SIZE.sm}
+                    color={COLORS.gray1}
+                    className="leading-6 pl-1"
+                  >
+                    {label}
+                  </Typography.Text>
+                  <div className="flex-1 border-t border-dashed border-gray-300"></div>
+                  <Typography.Text
+                    weight={FONT_WEIGHT.regular}
+                    size={FONT_SIZE.sm}
+                    className="leading-6 pr-1"
+                  >
+                    {value}
+                  </Typography.Text>
+                </div>
+              ))}
+            </div>
+          </div>
+        </section>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
---
+++ src/types.d.ts
@@ -0,0 +1,6 @@
+export interface LabelValuePair {
+  label: string;
+  value: string;
+}
+
+export type LabelValuePairs = LabelValuePair[];
